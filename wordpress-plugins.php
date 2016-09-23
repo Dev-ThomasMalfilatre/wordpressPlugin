e8380709c2585df3a3e00767061acf93ccf11e99
Thomas Malfilatre <dev.thomasmalfilatre@gmail.com>
2016-09-23 14:23:30 +0200

    Création d'un plugin wordpress d'ajout dans une base de données

---
+++ wordpress-plugins.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: CreateUser Plugin
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Create a new user in the database
+Version: 1.0
+Author: Thomas Malfilatre
+Author URI: dev.thomasmalfilatre@gmail.com
+License: GPL2
+*/
